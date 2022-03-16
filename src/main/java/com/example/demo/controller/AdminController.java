906bd1bb68342097d3e720aed4589855e0bbc83f
robbert-sudo <robbert_de_Die@hotmail.com>
2022-03-16 21:20:30 +0100

    cleanup

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -1,7 +1,6 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.response.UserDetailsResponse;
-import com.example.demo.model.User;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
