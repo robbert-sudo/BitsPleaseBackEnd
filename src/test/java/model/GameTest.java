60455f312eaddbbfd8e87bef3276bea921275f1d
robbert-sudo <robbert_de_Die@hotmail.com>
2022-08-10 21:24:56 +0200

    GameRepositoryTest

--- src/test/java/model/GameTest.java
+++ src/test/java/model/GameTest.java
@@ -1,4 +1,4 @@
-package Model;
+package model;
 
 import bitspleaseApp.model.Game;
 import org.junit.jupiter.api.Test;
--- src/test/java/model/UserTest.java
+++ src/test/java/model/UserTest.java
@@ -1,4 +1,4 @@
-package Model;
+package model;
 
 import bitspleaseApp.model.Authority;
 import bitspleaseApp.model.Game;
