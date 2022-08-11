74c9e49b3e89b538baf3b28886883519a49788d2
robbert-sudo <robbert_de_Die@hotmail.com>
2022-08-11 20:35:23 +0200

    admincontroller cleanup

--- src/main/java/bitspleaseApp/service/UserServiceImpl.java
+++ src/main/java/bitspleaseApp/service/UserServiceImpl.java
@@ -148,48 +148,6 @@ public class UserServiceImpl implements UserService {
         return new UserRateResponse(ratedUserName);
     }
 
-
-
-
-
-
-
-// onderstaande functies worden alleen in de admin controller gebruikt
-
-//    public Set<UserDetailsResponse> findAllByDisabled() {
-//        long user_id = 0;
-//        String username = null;
-//        boolean enabled = false;
-//        String email = null;
-//        Set<Authority> authorities = null;
-//        Set<Game> games = null;
-//        Set<UserDetailsResponse> userDetailsResponses = new HashSet<>();
-//
-//
-//        Iterable<User> users = userRepository.findAllByEnabled(false);
-//        for (User user : users) {
-//            user_id = user.getUser_id();
-//            username = user.getUsername();
-//            enabled = user.isEnabled();
-//            email = user.getEmail();
-//            authorities = user.getAuthorities();
-//            games = user.getGames();
-//            UserDetailsResponse userDetailsResponse = new UserDetailsResponse(user_id, username, enabled, email, authorities, games);
-//            userDetailsResponses.add(userDetailsResponse);
-//        }
-//        return userDetailsResponses;
-//    }
-
-//    @Override
-//    public void delete(String username) {
-//        Optional<User> possibleUser = userRepository.findByUsername(username);
-//        if (possibleUser.isPresent()) {
-//            User user = possibleUser.get();
-//            userRepository.delete(user);
-//        }
-//    }
-
-
 }
 
 
